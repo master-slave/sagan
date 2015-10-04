ade70d4399db88f65d21054faf8616721253c307
Slavisa Avramovic <slavisaavramovic@yahoo.com>
2015-10-04 08:32:46 +0200

    adding war plugin, changing SiteMain

--- sagan-site/src/main/java/sagan/SiteMain.java
+++ sagan-site/src/main/java/sagan/SiteMain.java
@@ -1,11 +1,22 @@
 package sagan;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
 /**
  * The entry point for the Sagan web application.
  */
-public class SiteMain {
+public class SiteMain extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         new SaganApplication(SiteConfig.class).run(args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        // Customize the application or call application.sources(...) to add sources
+        // Since our example is itself a @Configuration class we actually don't
+        // need to override this method.
+        return application.sources(SiteMain.class);
     }
 }
